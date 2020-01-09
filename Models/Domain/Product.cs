660d4ee356c8fd7e6d583bf4c65ff8c5b8a760dd
Jacob Holloway <Jacob.holloway94@yahoo.com>
2020-01-09 15:12:33 -0800

    Product page update.

--- Models/Domain/Product.cs
+++ Models/Domain/Product.cs
@@ -9,5 +9,11 @@ namespace CBDistro.Models.Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public string Brand { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
     }
 }

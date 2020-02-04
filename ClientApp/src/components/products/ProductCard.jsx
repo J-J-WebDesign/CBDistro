4442f1734dd0fdfce3641fa1a305836e58aa8307
Jacob Holloway <Jacob.holloway94@yahoo.com>
2020-02-04 14:30:11 -0800

    product card updates

--- ClientApp/src/components/products/ProductCard.jsx
+++ ClientApp/src/components/products/ProductCard.jsx
@@ -8,15 +8,29 @@ const ProductCard = props => {
   };
   return (
     <div className="card">
-      <h5 className="pt-4">{props.product.name}</h5>
+      <div className="row col-12 m-auto">
+        <i
+          className="mr-auto fa fa-2x fa-trash-o pt-3 trash cardButtons"
+          aria-hidden="true"
+        ></i>
+        <h5 className="pt-3 m-auto">
+          {props.product.name.length > 35
+            ? props.product.name.slice(0, 35)
+            : props.product.name}
+        </h5>
+        <i
+          className="ml-auto fa fa-cog fa-2x pt-3 gear cardButtons"
+          aria-hidden="true"
+        ></i>
+      </div>
       <hr />
       <img
-        className="mx-auto productImg rounded d-block"
+        className="mx-auto productImg rounded d-block mb-1"
         onError={onImageError}
         src={props.product.image}
-        alt={props.product.description.slice(0, 50)}
+        alt={props.product.description.slice(0, 30)}
       />
-      <h6>{props.product.brand}</h6>
+      <h6 className="mt-1">{props.product.brand}</h6>
       <hr />
       <h5>${props.product.price}</h5>
       <hr />

8408850e4f4195ec877417fa79072a3b1e39974a
jake <jakedenham70@gmail.com>
2020-02-04 12:32:02 -0800

    Insert product C#

---
+++ CBDistro.Models/Requests/ProductAddRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CBDistro.CBDistro.Models.Requests
+{
+    public class ProductsAddRequest
+    {
+    }
+}

90b14bca762a1b8a7b332644865e8b83a510b691
jake <jakedenham70@gmail.com>
2020-02-03 16:39:40 -0800

    merging

--- ClientApp/src/components/products/Products.jsx
+++ ClientApp/src/components/products/Products.jsx
@@ -1,40 +1,45 @@
-import React, { Component } from 'react'
-import * as productServices from "../services/productServices"
-import ProductCard from "./ProductCard"
+import React, { Component } from "react";
+import * as productServices from "../services/productServices";
+import ProductCard from "./ProductCard";
 import "./Product.css";
 
 export default class Products extends Component {
-constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-        products: null
-    }
-}
+      products: null
+    };
+  }
 
-    componentDidMount(){
-        productServices.getAll()
-        .then(this.onGetAllSuccess)
-        .catch(this.axiosFail)
-    }
+  componentDidMount() {
+    productServices
+      .getAll()
+      .then(this.onGetAllSuccess)
+      .catch(this.axiosFail);
+  }
 
-    onGetAllSuccess = data => {
-        let vat = data.items.map(this.productMapper)
-        this.setState(prevState=>{return{...prevState, products: vat}})
-    }
+  onGetAllSuccess = data => {
+    let vat = data.items.map(this.productMapper);
+    this.setState(prevState => {
+      return { ...prevState, products: vat };
+    });
+  };
 
-    productMapper = (product) => (<ProductCard key={product.id} product={product}/>)
+  productMapper = product => <ProductCard key={product.id} product={product} />;
 
-    goBack = ()=>{
-        this.props.history.push("/")
-    }
+  goBack = () => {
+    this.props.history.push("/");
+  };
 
-    render() {
-        return (
-            <div className="container col-12" id="products">
-                <h3>Products!</h3>
-                {this.state.products}
-                <button onClick={this.goBack} className="btn btn-danger">Go Home!</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container col-12" id="products">
+        <h3>Products!</h3>
+        {this.state.products}
+        <button onClick={this.goBack} className="btn btn-danger">
+          Go Home!
+        </button>
+      </div>
+    );
+  }
 }

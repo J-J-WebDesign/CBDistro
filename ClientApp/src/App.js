38e79e0edb259b5bb76806e7c50361ca93df2e57
Jacob Holloway <Jacob.holloway94@yahoo.com>
2020-02-04 12:44:35 -0800

    Revert "Merge branch 'master' into jaked"
    This reverts commit db677d5f63aa6ed5b663ee78b8fff5f860353160, reversing
    changes made to 90b14bca762a1b8a7b332644865e8b83a510b691.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,26 +1,22 @@
-import React from "react";
-import "./App.css";
-import Home from "./components/home/Home";
-import { Route } from "react-router-dom";
-import Products from "./components/products/Products";
-import Navbar from "./components/common/Navbar";
-import Cart from "./components/cart/Cart";
-import Sales from "./components/sales/Sales";
-import ProductForm from "./components/products/AddProductForm";
+import React from 'react';
+import './App.css';
+import Home from "./components/home/Home"
+import {Route}from "react-router-dom"
+import Products from "./components/products/Products"
+import Navbar from "./components/Navbar"
+import Cart from "./components/cart/Cart"
+import Sales from "./components/sales/Sales"
 
 function App() {
   return (
     <div className="App">
-      <div className="backGround">
-        <div className="navbarSpacing">
-          <Navbar />
-        </div>
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/products" component={Products} exact={true} />
-        <Route path="/products/add" component={ProductForm} exact={true} />
-        <Route path="/sales" component={Sales} exact={true} />
-        <Route path="/cart" component={Cart} exact={true} />
+      <div className="navbarSpacing">
+      <Navbar/>
       </div>
+    <Route path="/" component={Home} exact={true} />
+    <Route path="/products" component={Products} exact={true} />
+    <Route path="/sales" component={Sales} exact={true} />
+    <Route path="/cart" component={Cart} exact={true} />
     </div>
   );
 }
--- ClientApp/src/components/Navbar.jsx
+++ ClientApp/src/components/Navbar.jsx
@@ -1,70 +1,42 @@
-import React from "react";
-import "./Navbar.css";
+import React from 'react';
+import "./Navbar.css"
 
-const Navbar = props => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand brand" href="/">
-        CBDistro
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="/products">
-              Products
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link " href="/sales">
-              Sales
-            </a>
-          </li>
-          <li className="nav-item dropdown show">
-            <span
-              className="nav-link dropdown-toggle"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown
-            </span>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="/1">
-                Action
-              </a>
-              <a className="dropdown-item" href="/2">
-                Another action
-              </a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="/3">
-                Something else here
-              </a>
-            </div>
-          </li>
-        </ul>
-        <ul className="navbar-nav">
-          <li className="ml-auto nav-item">
-            <a className="nav-link" href="/cart">
-              <i className="fas fa-lg fa-cart-plus cart" />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+const Navbar = (props) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a className="navbar-brand brand" href="/">CBDistro</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <a className="nav-link" href="/products">Products</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link " href="/sales" >Sales</a>
+      </li>
+      <li className="nav-item dropdown show">{
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
+        }<a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="/1">Action</a>
+          <a className="dropdown-item" href="/2">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="/3">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <ul className="navbar-nav">
+      <li className="ml-auto nav-item">
+      <a className="nav-link" href="/cart"><i className="fas fa-lg fa-cart-plus cart"/></a>
+      </li>
+    </ul>
+  </div>
+  </nav>
+    );
 };
 
 export default Navbar;
